472677355419fcbb4855cc08b940e66f2f685249
Stanislau.Komar <stanislau_komar@epam.com>
2022-01-17 06:56:13 +0300

    v0.1

---
+++ src/Business/SymfonySerializerDecorator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Micro\Plugin\Serializer\Adapter\Symfony\Business;
+
+use Micro\Plugin\Serializer\Business\SerializerInterface;
+use Micro\Plugin\Serializer\SerializerFacadeInterface;
+use Symfony\Component\Serializer\SerializerInterface as SymfonySerializerInterface;
+
+class SymfonySerializerDecorator implements SerializerInterface
+{
+
+    private const FORMAT_COLL = [
+        SerializerFacadeInterface::FORMAT_JSON => 'json',
+        SerializerFacadeInterface::FORMAT_XML   => 'xml',
+    ];
+
+    public function __construct(private SymfonySerializerInterface $serializer)
+    {
+    }
+
+    /**
+     * @param object $object
+     * @param string $format
+     * @param array $context
+     * @return string
+     */
+    public function serialize(object $object, string $format, array $context = []): string
+    {
+        return $this->serializer->serialize($object, self::FORMAT_COLL[$format], $context);
+    }
+
+    /**
+     * @param string $data
+     * @param string $objectClass
+     * @param string $format
+     * @param array $options
+     * @return object
+     */
+    public function deserialize(string $data, string $objectClass, string $format, array $options = []): object
+    {
+        return $this->serializer->deserialize($data, $objectClass, $format, $options);
+    }
+}
